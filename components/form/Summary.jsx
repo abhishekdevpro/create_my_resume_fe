9a7747cde1b618085d936be68431add483e7cbb9
Anita Chouhan <fe.dev2.ba2024@gmail.com>
2025-02-11 16:00:27 +0530

    update code

--- components/form/Summary.jsx
+++ components/form/Summary.jsx
@@ -445,13 +445,14 @@ const Summary = () => {
       </div>
 
       {/* ReactQuill Editor */}
-      <div className="grid-1 w-full">
+      <div className="grid-1 w-full notranslate">
         <ReactQuill
           placeholder="Enter your professional summary or use Smart Assist to generate one"
           value={resumeData.summary || ""}
           onChange={handleQuillChange}
           className="w-full other-input h-100 border-black border rounded"
           theme="snow"
+          preserveWhitespace={true}
           modules={{
             toolbar: [["bold", "italic", "underline"], ["clean"]],
           }}
